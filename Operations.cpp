757857eb6b71f1ff23853abfa3a8933fd37be180
HarutiunHovsepyan <50136105+HarutiunHovsepyan@users.noreply.github.com>
2019-05-07 11:20:03 +0200

    Update Operations.cpp

--- Operations.cpp
+++ Operations.cpp
@@ -3,18 +3,18 @@
 int addition(int a, int b)
 {
 	int resultat;
-	
+
 	resultat = a + b;
-	
+
 	return resultat;
 }
 
 int soustraction(int a, int b)
 {
 	int resultat;
-	
+
 	resultat = a - b;
-	
+
 	return;
 }
 int division(int a, int b)
@@ -25,4 +25,13 @@ int division(int a, int b)
 int multiplication(int a,int b)
 {
     return(a*b);
+}
+
+int exposant(int nb, int exp);
+{
+    for (int i=0; i>exp; i--)
+    {
+        nb=nb*nb;
+    }
+    return(nb)
 }
