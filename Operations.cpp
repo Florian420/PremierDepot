83ba155b699c6f9feee57cf4e3e50991ad36805e
Florian420 <raimondiflorian@gmail.com>
2019-05-07 11:16:45 +0200

    Factoriel
    cpp

--- Operations.cpp
+++ Operations.cpp
@@ -3,18 +3,18 @@
 int addition(int a, int b)
 {
 	int resultat;
-	
+
 	resultat = a + b;
-	
+
 	return resultat;
 }
 
 int soustraction(int a, int b)
 {
 	int resultat;
-	
+
 	resultat = a - b;
-	
+
 	return;
 }
 int division(int a, int b)
@@ -25,4 +25,14 @@ int division(int a, int b)
 int multiplication(int a,int b)
 {
     return(a*b);
+}
+
+int factoriel(int nb)
+{
+    int result=nb;
+    for (int i=nb;i>0;i--)
+    {
+        result = result*i;
+    }
+    return result;
 }
